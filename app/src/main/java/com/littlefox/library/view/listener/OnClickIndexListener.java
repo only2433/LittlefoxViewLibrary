99517e7f66bc36f01bbbe93756be7b57bcf54450
only2433 <only2433@naver.com>
2017-08-11 16:32:08 +0900

    추가

--- app/src/main/java/com/littlefox/library/view/listener/OnClickIndexListener.java
+++ app/src/main/java/com/littlefox/library/view/listener/OnClickIndexListener.java
@@ -2,6 +2,10 @@ package com.littlefox.library.view.listener;
 
 import android.view.View;
 
+/**
+ * 인덱스를 전달 하기 위한 클릭 리스너
+ * by 정재현
+ */
 public abstract class OnClickIndexListener implements View.OnClickListener
 {
 	protected int index;
